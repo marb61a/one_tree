3785d9fb89db6dfa3bc6e2bc1eae4beeecda3509
marb <marb61a@gmail.com>
2016-11-09 09:57:41 +0000

    added initial main.js

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 970 ){
+            $('.navbar').addClass('navbar-fixed-top');    
+        }
+        
+        if($(window).scrollTop() < 971 ){
+            $('.navbar').removeClass('navbar-fixed-top');     
+        }
+    });
+});
+
+$(".navbar-dark ul li a[href^='#']").on('click', function(e){
+    e.preventDefault();
+    var hash = this.hash;
+    
+    $('html', 'body').animate({
+        scrollTop: $(hash).offset().top
+    }, 300, function(){
+        window.location.hash = hash;
+    });
+    
+});
+
+$("#section-parallax .col-sm-8 a[href^='#']").on('click', function(e){
+    e.preventDefault();
+    var hash = this.hash;
+    
+    $('html', 'body').animate({
+        scrollTop: $(hash).offset().top
+    }, 300, function(){
+        window.location.hash = hash;
+    });
+    
+});
