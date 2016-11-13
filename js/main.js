7706219dcbf6e4efeab8f338622baf068de4d7d3
marb <marb61a@gmail.com>
2016-11-13 13:34:53 +0000

    updated main.js and added footer to index.html

--- js/main.js
+++ js/main.js
@@ -33,3 +33,15 @@ $("#section-parallax .col-sm-8 a[href^='#']").on('click', function(e){
     });
     
 });
+
+$(".footer .scroll-to-top-button a[href^='#']").on('click', function(e) {
+    e.preventDefault();
+    var hash = this.hash;
+    
+    $('html', 'body').animate({
+        scrollTop: $(hash).offset().top
+    }, 300, function(){
+        window.location.hash = hash;
+    });
+});
+
